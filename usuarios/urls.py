cfa06ee3925ecf9f227b5f63ecb1a34553db369c
suedescr <dev.suede@gmail.com>
2024-08-30 20:40:35 -0400

    Aula 30-08-2024

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('login/', views.login , name = 'login'), 
-    path('cadastro/', views.cadastro, name = 'cadastro'),
+    path('login/', views.login, name='login'),
+    path('cadastro/', views.cadastro, name='cadastro'),
     path('home/', views.home, name = 'home'),
     path('lancar/', views.lancar, name = 'lancar'),
     path('alterar/', views.alterar, name = 'alterar'),
     path('visualizar/', views.visualizar, name = 'visualizar'),
+    path('logout/', views.logout, name = 'logout')
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,56 +2,88 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from django.contrib.auth import login as login_django
+from django.contrib.auth import login as login_django, logout as logout_django
+from .models import Nota
 
-def login(request):
+
+
+
+def login (request):
     if request.method == "GET":
         return render(request, 'usuarios/login.html')
     else:
-        username = request.POST.get('email')
-        senha = request.POST.get('senha')
-
-        user = authenticate(username=username, password=senha)
-
-        if user: 
-            login_django(request, user)
-            return HttpResponse('Autenticado!')
-        else:
-            return HttpResponse('E-mail ou senha inválidos!')
-    
-def cadastro(request):
-    if request.method == "GET":
-        return render(request, 'usuarios/cadastro.html')
-    else:
-        username = request.POST.get('email')
-        email = request.POST.get('email')
-        password = request.POST.get('senha')
-        first_name = request.POST.get('nome')
-
-        user = User.objects.filter(username=username).first()
-
+        username = request.POST.get ('email')
+        senha =  request.POST.get ('senha')
+        
+        user = authenticate (username = username, password = senha)
+        
         if user:
-            return HttpResponse("Usuário já existente!")
+            login_django(request, user)
+            return render(request, 'usuarios/home.html')
         else:
-            User.objects.create_user(username=username, email=email, password=password, first_name=first_name)
-            return HttpResponse("Usuário cadastrado com sucesso!")
+            return HttpResponse('E-mail ou senha invalidos!')
 
+def cadastro(request):
+  if request.method == "GET":
+    return render(request, 'usuarios/cadastro.html')
+  else:
+    username = request.POST.get('email')
+    email = request.POST.get('email')
+    password = request.POST.get('senha')
+    first_name = request.POST.get('nome')
+
+    user =  User.objects.filter(username=username).first()
+
+    if user:
+      return HttpResponse("Usuário já existente!")
+    else:
+      user = User.objects.create_user(username=username,email=email,password=password,first_name=first_name)
+      user.save()
+
+      return render(request, 'usuarios/login.hml')
+    
 def home(request):
+  if request.user.is_authenticated:
     return render(request, 'usuarios/home.html')
+  else:
+    return HttpResponse("Faça o login para acessar!")
+  
 def lancar(request):
+  if request.method == "GET":
     if request.user.is_authenticated:
-        return render(request, 'usuarios/home.html')
-    else:
-        return HttpResponse("Faça o Login para acessar!!")
+      return render(request, 'usuarios/lancar.html')
+  else:
+    return HttpResponse("Faça o login para acessar!")
+else:
+    nota = Nota()
+    nota.nome_aluno = request.user.first_name
+    nota.disciplina = request.POST.get('disciplina')   #Este post é da requisição  e o get minusculo é o verbo pegar
+    nota.nota_atividades = request.POST.get('nota_atividades')
+    nota.nota_trabalho =  request.POST.get('nota_trabalho')
+    nota.nota_prova = request.POST.get('nota_prova')
+    nota.media = int(nota.nota_atividades) + int(nota.nota_trabalho) + int(nota.nota_prova)
 
+    nota_verificada = Nota.objects.filter(disciplina = nota.disciplina).first()
+    if nota_verificada:
+        return HttpResponse("Disciplina já possui nota cadastrada!")
+    else:
+   nota.save()
+    return render(request, "usuarios/home.html")
 def alterar(request):
-    if request.user.is_authenticated:
-        return render(request, 'usuarios/alterar.html')
-    else:
-        return HttpResponse("Faça o Login para acessar!!")
-
+  if request.user.is_authenticated:
+    return render(request, 'usuarios/alterar.html')
+  else:
+    return HttpResponse("Faça o login para acessar!")
 def visualizar(request):
-    if request.user.is_authenticated:
-        return render(request, 'visualizar/home.html')
-    else:
-        return HttpResponse("Faça o Login para acessar!!")
+  if request.user.is_authenticated:
+    return render(request, 'usuarios/visualizar.html')
+  else:
+    return HttpResponse("Faça o login para acessar!")
+
+
+def logout(request):
+    if request.user.is_authenticated: 
+      logout_django(request)
+      return render(request, 'usuarios/login.html')
+    else: 
+      return HttpResponse("Você não acessou sua conta ainda!")
